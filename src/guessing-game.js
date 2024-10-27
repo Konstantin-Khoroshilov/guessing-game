852d8e672ff708bb9f5978c701e5ed773f23bb40
Konstantin Khoroshilov <manly41142009@yandex.ru>
2024-10-27 18:00:55 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,20 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  constructor() { }
+  min;
+  max;
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+  guess() {
+    return Math.ceil((this.min + this.max) / 2);
+  }
+  lower() {
+    this.max = Math.ceil((this.min + this.max) / 2);
+  }
+  greater() {
+    this.min = Math.ceil((this.min + this.max) / 2);
+  }
 }
 
 module.exports = GuessingGame;
